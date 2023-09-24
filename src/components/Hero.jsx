fbb2f98d115682c592f47c91f8c1b1916c148c95
Zakaria Zhlat <zakooorr1567@gmail.com>
2023-09-24 13:14:22 +0300

    first deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Zakaria </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I'M A WEB DEVELOPER   <br className='sm:block hidden' />
+          I'M A FullStack DEVELOPER   <br className='sm:block hidden' />
           EXPERIENCED IN REACT, DJANGO, VUE AND LARAVEL.
           </p>
         </div>
